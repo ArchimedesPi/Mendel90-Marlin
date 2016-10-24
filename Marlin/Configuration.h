b7b6a0fb530685d1a12eb8b13667b159afce9410
Liam Marshall <liam@cpan.org>
2016-10-23 22:05:37 -0500

    Fix steps/mm for Z axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -509,7 +509,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,6400,E_STEPS_PER_MM}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,5120,E_STEPS_PER_MM}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {400, 400, 4, 30}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {2000,2000,150,5000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
